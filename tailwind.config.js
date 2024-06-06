db7a3dd395de529d9904de38b4fc771beef2ce19
Isabelle Gustafsson <isabellegustafsson@Isabelles-Air.lan>
2024-06-06 13:14:25 +0200

    added daisyUI custom theme and footer component

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,24 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#a991f7",
+          "secondary": "#f6d860",
+          "accent": "#37cdbe",
+          "neutral": "#3d4451",
+          "base-100": "#ffffff",
+          "info": "#2094f3",
+          "success": "#009485",
+          "warning": "#ff9900",
+          "error": "#ff5724",
+        },
+      },
+      // Add more themes here if needed
+    ],
+  },
   plugins: [
     require('daisyui'),
   ],
