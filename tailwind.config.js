bfb0185bebe728e97898ab1afc6a68f4ee186aa1
Isabelle Gustafsson <isabellegustafsson@Isabelles-MacBook-Air.local>
2024-06-07 10:34:40 +0200

    added content in about and contact components

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,13 +10,14 @@ module.exports = {
         mytheme: {
           primary: "#67e8f9",
           secondary: "#06b6d4",
-          accent: "#fb923c",
+          accent: "#FAA300",
           neutral: "#f5f5f4",
           "base-100": "#fffcff",
           info: "#0000ff",
           success: "#00ff00",
           warning: "#00ff00",
           error: "#ff0000",
+          white: "#F5FAF9",
         },
       },
       // Add more themes here if needed
